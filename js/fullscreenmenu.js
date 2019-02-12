25ac857203adda9c925ef6edc591b8d9ccae3f0d
Mykhailo Brych <brychmykhailo@gmail.com>
2019-02-12 10:25:06 +0200

    Поправил ДЗ 3, доделал моб меню

--- js/fullscreenmenu.js
+++ js/fullscreenmenu.js
@@ -1,13 +1,15 @@
 
 document.querySelector('.mobile__menu__btn').addEventListener("click", function() {
-    console.log('нажал')
     let element = document.querySelector('.fullscreen__menu');
     element.classList.add('fullscreen__menu-active');
+    let body = document.querySelector('body');
+    body.classList.add('body-fixed');
 })
 
 
 document.querySelector('.mobile__menu__btn-active').addEventListener("click", function() {
-    console.log('удалил')
     let element = document.querySelector('.fullscreen__menu');
     element.classList.remove('fullscreen__menu-active');
+    let body = document.querySelector('body');
+    body.classList.remove('body-fixed');
 })
